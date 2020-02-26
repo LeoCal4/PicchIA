4e61ac9c747771854365dc8265a35f1de9aae98b
LeoCal4 <linkindayd@gmail.com>
2020-02-27 00:52:55 +0100

    Added (bad) aiming and weapons

--- Player.py
+++ Player.py
@@ -11,12 +11,13 @@ class Player(pygame.sprite.Sprite):
 
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.surface.Surface((self.WIDTH, self.HEIGTH))
+        #self.image = pygame.surface.Surface((self.WIDTH, self.HEIGTH))
         self.image = pygame.image.load('pg.png')
+        #self.image.fill(WHITE)
         self.original_image = self.image
         self.rect = self.image.get_rect()
         self.hp = 100
-        self.weapons = [weapon.Shotgun(self)]
+        self.weapons = [weapon.Shotgun(self), weapon.Assault_Rifle(self), weapon.Sniper_Rifle(self)]
         self.current_weapon = self.weapons[0]
         self.move_dir_x = 0
         self.move_dir_y = 0
@@ -30,14 +31,16 @@ class Player(pygame.sprite.Sprite):
         self.move_dir_y = keys[pygame.K_s] - keys[pygame.K_w]
         self.move()
         self.rotate()
+        self.handle_weapon_change(keys)
         if keys[pygame.K_SPACE] and self.enable_shooting:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
             self.current_weapon.shoot()
             self.enable_shooting = False
         if not self.enable_shooting:
             self.delta_time_shooting += 1
         if self.delta_time_shooting % self.current_weapon.shoot_delay == 0:
             self.enable_shooting = True
-            self.delta_time_shooting += 0
+            self.delta_time_shooting = 0
         self.all_projectiles.update()
         self.all_projectiles.draw(pygame.display.get_surface())
 
@@ -51,3 +54,8 @@ class Player(pygame.sprite.Sprite):
         shooting_angle = (180 / pi) * -atan2(rel_y, rel_x)
         self.image = pygame.transform.rotate(self.original_image, int(shooting_angle))
         self.rect = self.image.get_rect(center=self.rect.center)
+
+    def handle_weapon_change(self, keys):
+        weapons_dict = {pygame.K_1 : 0, pygame.K_2 : 1, pygame.K_3 : 2}
+        for key in weapons_dict:
+            if keys[key]: self.current_weapon = self.weapons[weapons_dict[key]]
--- weapon.py
+++ weapon.py
@@ -1,4 +1,6 @@
 import pygame
+from math import sqrt, sin, cos, degrees
+import random 
 
 class _Weapon:
     def __init__(self, shoot_delay, projectile_speed, damage, player):
@@ -6,27 +8,47 @@ class _Weapon:
         self.projectile_speed = projectile_speed
         self.damage = damage
         self.player = player
-    
+
     def shoot(self):
-        proj = Projectile(self.player.rect.center, self)
-        proj.rect.right = self.player.rect.right
-        proj.rect.centery = self.player.rect.centery
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        direction_vector = pygame.math.Vector2(self._normalize_vector([mouse_x - self.player.rect.x, mouse_y - self.player.rect.y]))
+        proj = Projectile(self.player.rect.center, direction_vector, self)
         proj.add(self.player.all_projectiles)
 
-class Shotgun(_Weapon):
-    def __init__(self, player):
-        _Weapon.__init__(self, 100, 5, 20, player)
+    def _normalize_vector(self, vector):
+        norm = sqrt(sum(element * element for element in vector))
+        return [element / norm for element in vector]
 
+class Shotgun(_Weapon):
+    NUM_OF_SHOTS = 6
+    SPREAD = .2
+    def __init__(self, player):
+        super().__init__(250, 5, 20, player)
+
+    def shoot(self):
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        base_direction_vector = pygame.math.Vector2(self._normalize_vector([mouse_x - self.player.rect.x, mouse_y - self.player.rect.y]))
+        for i in range(self.NUM_OF_SHOTS):
+            randomized_direction_vector = base_direction_vector + pygame.math.Vector2([random.uniform(-self.SPREAD, self.SPREAD), random.uniform(-self.SPREAD, self.SPREAD)])
+            proj = Projectile(self.player.rect.center, randomized_direction_vector, self)
+            proj.add(self.player.all_projectiles)
 
 class Sniper_Rifle(_Weapon):
     def __init__(self, player):
-        super.__init__(self, 50, 10, 40, player)
+        super().__init__(300, 10, 40, player)
 
 
 class Assault_Rifle(_Weapon):
+    SPREAD = .05
     def __init__(self, player):
-        super.__init__(self, 5, 15, 10, player)
+        super().__init__(40, 15, 10, player)
 
+    def shoot(self):
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        direction_vector = pygame.math.Vector2(self._normalize_vector([mouse_x - self.player.rect.x, mouse_y - self.player.rect.y]))
+        randomized_direction_vector = direction_vector + pygame.math.Vector2([random.uniform(-self.SPREAD, self.SPREAD), random.uniform(-self.SPREAD, self.SPREAD)])
+        proj = Projectile(self.player.rect.center, randomized_direction_vector, self)
+        proj.add(self.player.all_projectiles)
 
 class Projectile(pygame.sprite.Sprite):
     PROJECTILE_COLOR = (255, 255, 255)
@@ -34,15 +56,20 @@ class Projectile(pygame.sprite.Sprite):
     CENTER = (RADIUS, RADIUS)
     SURFACE_SIZE = (RADIUS*2, RADIUS*2)
 
-    def __init__(self, position, weapon):
+    def __init__(self, position, direction_vector, weapon):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.surface.Surface(self.SURFACE_SIZE)
         pygame.draw.circle(self.image, self.PROJECTILE_COLOR, self.CENTER, self.RADIUS)
         self.rect = self.image.get_rect(center = position)
+        self.position = pygame.math.Vector2(position)
         self.speed = weapon.projectile_speed
+        self.direction = direction_vector
         self.screen = pygame.display.get_surface()
 
     def update(self):
-        self.rect.x += self.speed
+        self.position += self.speed * self.direction
+        self.rect.center = self.position
         if not self.screen.get_rect().colliderect(self.rect):
             self.kill()
+    
+    
